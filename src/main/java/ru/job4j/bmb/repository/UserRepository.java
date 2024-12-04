a5c112f06d38716da7a174a6071eb1f4e1ffa3a5
admin <desantt8@gmail.com>
2024-12-04 17:18:42 +0300

    Spring Data JPA [#505298]

--- src/main/java/ru/job4j/bmb/repository/UserRepository.java
+++ src/main/java/ru/job4j/bmb/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package ru.job4j.bmb.repository;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import ru.job4j.bmb.model.User;
@@ -8,10 +9,6 @@ import java.util.List;
 
 @Component
 @Repository
-public interface UserRepository {
+public interface UserRepository extends CrudRepository<User, Long> {
     List<User> findAll();
-
-    User findByClientId(Long clientId);
-
-    public void save(User user);
 }
