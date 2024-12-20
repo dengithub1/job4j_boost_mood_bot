493081b58be8bdfc7fb3905643205647147083c3
admin <desantt8@gmail.com>
2024-12-20 11:42:01 +0300

    deleted class

--- src/main/java/ru/job4j/bmb/ContentProvider.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.bmb;
-
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
-
-@Component
-@Controller
-public interface ContentProvider {
-}
